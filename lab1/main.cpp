22b2d31bb70e2c274f3cd9741cae14bc446b83ff
aleksandrzuravl <st129760@student.spbu.ru>
2025-09-22 22:40:57 +0300

    rework main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,24 +1,39 @@
-#include "reader_bmp.h"
+#include "bmp_reader.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
 
+    // Check heving two arguments
+    if (argc < 2)
+    {
+        std::cout << "Using: " << argv[0] << " <path to BMP file>" << "\n"; // If the file path is not specified
+        return 0;
+    }
+
     BMP_File bmp_file;
+    if (!bmp_file.Load_BMP_File(argv[1]))  // Create array for BMP imagine
+    {
+        std::cerr << "Failed to load BMP file." << "\n";
+        return -1;
+    }
+    else
+    {
+        std::cout << "BMP File read succesful." << "\n";
+    }
     
     
     std::cout << "BMP file size:" << bmp_file.bmp_header.file_size << " byte" << "\n";
+    std::cout << "-> First task completed: file size printed." << "\n\n";
     
-    // rotate BMP contra clockwise
-    auto new_bmp_file_1 = bmp_file.rotate_contra_clockwise();
-    new_bmp_file_1->Save_image("BMP_contra_clockwise.bmp");
-
-
-
+    // Flip BMP contra clockwise
+    auto new_bmp_file_1 = bmp_file.flip_BMP_90_contra_clockwise();
+    new_bmp_file_1->Save_BMP_File("BMP_contra_clockwise.bmp"); // Save new file 1
+    std::cout << "-> Second task completed: file flip contra clockwise." << "\n";
     
-    // rotate BMP clockwise
-    auto new_bmp_file_2 = bmp_file.rotate_clockwise();
-    new_bmp_file_2->Save_image("BMP_clockwise.bmp"); // Save new file 2
-
+    // Flip BMP clockwise
+    auto new_bmp_file_2 = bmp_file.flip_BMP_90_clockwise();
+    new_bmp_file_2->Save_BMP_File("BMP_clockwise.bmp"); // Save new file 2
+    std::cout << "-> Third task completed: file flip clockwise." << "\n";
 
     return 0;
 }
